09ddc8c5b78354b8a025fce588e49b7bbdcf20fd
Slindokuhle-Duma <slindokuhle08@gmail.com>
2022-06-30 10:15:01 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
-
-/
-
-* main - Entry point
-*
-* * Return: Always 0 (sucess)
-*
-* */
-
-int main(void)
-
-{
+int main(){
+	
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 
 }
