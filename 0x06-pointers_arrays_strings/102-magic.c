c15f64fa881cb95df31faf6e2cf3833ceec60879
Slindokuhle-Duma <106927068+Slindokuhle-Duma@users.noreply.github.com>
2022-07-13 20:57:20 +0200

    error editting

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,16 +16,8 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-<<<<<<< HEAD
   *(p + 5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
-=======
-	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
-}
->>>>>>> 7f044868939bbdaf73f88dd7950168fd7983770f
